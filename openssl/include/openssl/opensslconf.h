035cd138ff4aae4f5509afdc0de5356750ed8e1a
Yuriy Levytskyy <yuriy_levytskyy@epam.com>
2019-03-01 10:36:58 +0200

    Updated iOS OpenSSL build

--- openssl/include/openssl/opensslconf.h
+++ openssl/include/openssl/opensslconf.h
@@ -202,11 +202,21 @@ extern "C" {
  * The following are cipher-specific, but are part of the public API.
  */
 #if !defined(OPENSSL_SYS_UEFI)
-# undef BN_LLONG
+# ifdef __LP64__
+#  undef BN_LLONG
+# else
+#  define BN_LLONG
+# endif
 /* Only one for the following should be defined */
-# define SIXTY_FOUR_BIT_LONG
-# undef SIXTY_FOUR_BIT
-# undef THIRTY_TWO_BIT
+# ifdef __LP64__
+#  define SIXTY_FOUR_BIT_LONG
+#  undef SIXTY_FOUR_BIT
+#  undef THIRTY_TWO_BIT
+# else
+#  undef SIXTY_FOUR_BIT_LONG
+#  undef SIXTY_FOUR_BIT
+#  define THIRTY_TWO_BIT
+# endif
 #endif
 
 #define RC4_INT unsigned char
